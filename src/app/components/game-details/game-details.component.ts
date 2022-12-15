828bdb981627bc4f82251d5043e2e35b3ee1f6a4
Daniele Tortora <tortora87@gmail.com>
2022-12-15 17:14:35 +0100

    add rating gauge

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,7 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
-import { tap } from 'rxjs';
+import {
+  delay,
+  filter,
+  map,
+  merge,
+  Observable,
+  of,
+  startWith,
+  tap,
+} from 'rxjs';
 import { GameDetailsFacade } from 'src/app/services/game-details.facade';
 
 @Component({
@@ -15,6 +24,15 @@ export class GameDetailsComponent implements OnInit {
     tap((game) => game && this.title.setTitle(`${game.name} | Videogames DB`))
   );
   loading$ = this.facade.loadingGame$;
+
+  rating$: Observable<number> = merge(
+    of(0.1),
+    this.game$.pipe(
+      filter(Boolean),
+      map((game) => game.metacritic),
+      delay(250)
+    )
+  );
 
   constructor(
     private router: Router,
@@ -31,5 +49,17 @@ export class GameDetailsComponent implements OnInit {
         this.facade.findGame(params['id']);
       }
     });
+  }
+
+  calcRatingColor(value: number): string {
+    if (value > 75) {
+      return '#5ee432';
+    } else if (value > 50) {
+      return '#fffa50';
+    } else if (value > 30) {
+      return '#f7aa38';
+    } else {
+      return '#ef4655';
+    }
   }
 }
